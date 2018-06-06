0c4a8199d2d60032fd7569a17b086057d4f62533
zhaoyunxing <2385585770@qq.com>
2018-06-07 00:35:30 +0800

    添加:程序启动类

---
+++ src/main/java/com/sunny/boot/cherrytomato/CherryTomatoApplication.java
@@ -0,0 +1,12 @@
+package com.sunny.boot.cherrytomato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryTomatoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CherryTomatoApplication.class, args);
+  }
+}
