97ea8c330282db5396e78b6eb2dcfc8d166a1f19
zhaoyunxing <2385585770@qq.com>
2018-06-08 18:30:58 +0800

    添加：拦截器

--- src/main/java/com/sunny/boot/cherrytomato/core/interceptor/AppGlobalHandlerInterceptor.java
+++ src/main/java/com/sunny/boot/cherrytomato/core/interceptor/AppGlobalHandlerInterceptor.java
@@ -1,10 +1,56 @@
 package com.sunny.boot.cherrytomato.core.interceptor;
 
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * @author sunny
  * @class: com.sunny.boot.cherrytomato.core.interceptor.AppGlobalHandlerInterceptor
  * @date: 2018-06-08 13:54
- * @des:
+ * @des: 全局拦截器
  */
-public class AppGlobalHandlerInterceptor {
+public class AppGlobalHandlerInterceptor implements HandlerInterceptor {
+  private static final String[] EXCLUDES = {"/swagger-resources", "/configuration/security", "/v2/api-docs", "/configuration/ui", "/user", "/error"};
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    String path = request.getServletPath();
+
+    if (isInterceptor(path)) {
+      response.setContentType("application/json; charset=utf-8");
+      response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      response.getWriter().write("请先登录吧！");
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+  }
+
+  /**
+   * 是否拦截path ,默认都拦截
+   *
+   * @param path
+   * @return
+   */
+  private boolean isInterceptor(String path) {
+    for (String exclude : EXCLUDES) {
+      if (path.startsWith(exclude)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
