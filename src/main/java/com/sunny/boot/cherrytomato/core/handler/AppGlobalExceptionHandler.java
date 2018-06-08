a455752016ab81acec4f759117e0f3383e7cf2f3
zhaoyunxing <2385585770@qq.com>
2018-06-08 18:27:55 +0800

    添加：统一异常处理

--- src/main/java/com/sunny/boot/cherrytomato/core/handler/AppGlobalExceptionHandler.java
+++ src/main/java/com/sunny/boot/cherrytomato/core/handler/AppGlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.sunny.boot.cherrytomato.core.handler;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
 /**
  * @author sunny
  * @class: com.sunny.boot.cherrytomato.core.handler.AppGlobalExceptionHandler
  * @date: 2018-06-08 13:51
- * @des:
+ * @des: 统一异常处理
  */
+@ControllerAdvice
 public class AppGlobalExceptionHandler {
+
+
 }
