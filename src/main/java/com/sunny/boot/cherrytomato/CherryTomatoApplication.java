0f41d40b62a500d1b4d99eaf224ea695c142baf0
zhaoyunxing <2385585770@qq.com>
2018-06-08 14:30:23 +0800

    添加：跨域配置

--- src/main/java/com/sunny/boot/cherrytomato/CherryTomatoApplication.java
+++ src/main/java/com/sunny/boot/cherrytomato/CherryTomatoApplication.java
@@ -1,9 +1,12 @@
 package com.sunny.boot.cherrytomato;
 
+import com.sunny.boot.cherrytomato.core.config.AppWebMvcConfigurerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({AppWebMvcConfigurerAdapter.class})
 public class CherryTomatoApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/sunny/boot/cherrytomato/core/config/AppWebMvcConfigurerAdapter.java
+++ src/main/java/com/sunny/boot/cherrytomato/core/config/AppWebMvcConfigurerAdapter.java
@@ -3,11 +3,17 @@ package com.sunny.boot.cherrytomato.core.config;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import com.google.gson.GsonBuilder;
+import com.sunny.boot.cherrytomato.core.interceptor.AppGlobalHandlerInterceptor;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.CacheControl;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.WebContentInterceptor;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -20,15 +26,68 @@ import java.util.List;
  * @des:
  */
 public class AppWebMvcConfigurerAdapter implements WebMvcConfigurer {
-  @Bean
-  public HttpMessageConverters fastJsonHttpMessageConverters() {
+
+
+  /**
+   * 跨域配置
+   *
+   * @param registry
+   */
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**")
+        .allowedOrigins("*")
+        .allowCredentials(true)
+        .allowedMethods("GET", "POST", "DELETE", "PUT")
+        .maxAge(3600);
+
+  }
+
+  /**
+   * 拦截器配置
+   *
+   * @param registry
+   */
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+    CacheControl nocache = CacheControl.noCache();
+    webContentInterceptor.addCacheMapping(nocache, "/**");
+    registry.addInterceptor(webContentInterceptor);
+    //添加拦截器
+    registry.addInterceptor(new AppGlobalHandlerInterceptor()).addPathPatterns("/**");
+    WebMvcConfigurer.super.addInterceptors(registry);
+
+  }
+
+  /**
+   * url 参数配置
+   *
+   * @param configurer
+   */
+  @Override
+  public void configurePathMatch(PathMatchConfigurer configurer) {
+    //url支持 sunny.com 参数
+    configurer.setUseSuffixPatternMatch(false);
+  }
+
+  /**
+   * 添加gson解析器（过滤返回的数据排除为null的）
+   * <a href="http://blog.gelu.me/2017/spring-boot-uses-gson-to-replace-jackson/">参考地址</a>
+   *
+   * @param converters
+   */
+  @Override
+  public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+    converters.removeIf(httpMessageConverter -> httpMessageConverter instanceof MappingJackson2HttpMessageConverter); // 删除MappingJackson2HttpMessageConverter
+
     FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
     FastJsonConfig fastJsonConfig = new FastJsonConfig();
     SerializerFeature[] serializerFeatures = new SerializerFeature[]{
         //  输出key是包含双引号
 //        SerializerFeature.QuoteFieldNames,
         //  是否输出为null的字段,若为null 则显示该字段
-     //   SerializerFeature.WriteMapNullValue,
+        //   SerializerFeature.WriteMapNullValue,
         SerializerFeature.WriteNullNumberAsZero,
         SerializerFeature.WriteNullListAsEmpty,
         SerializerFeature.WriteNullStringAsEmpty,
@@ -41,11 +100,74 @@ public class AppWebMvcConfigurerAdapter implements WebMvcConfigurer {
     fastJsonConfig.setCharset(Charset.forName("UTF-8"));
     fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
     fastConverter.setFastJsonConfig(fastJsonConfig);
+    System.out.println("fastJsonConfig.getDateFormat()>>>" + fastJsonConfig.getDateFormat());
     HttpMessageConverter<?> converter = fastConverter;
     List<MediaType> mediaTypes = new ArrayList<>();
     mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);//设定json格式且编码为UTF-8
     fastConverter.setSupportedMediaTypes(mediaTypes);
 
-    return new HttpMessageConverters(converter);
+    converters.add(converter);
+
   }
+//  /**
+//   * 跨域配置
+//   *
+//   * @param registry
+//   */
+//  @Override
+//  public void addCorsMappings(CorsRegistry registry) {
+//    registry.addMapping("/**")
+//        .allowedOrigins("*")
+//        .allowCredentials(true)
+//        .allowedMethods("GET", "POST", "DELETE", "PUT")
+//        .maxAge(3600);
+//
+//  }
+//
+//  /**
+//   * 拦截器配置
+//   *
+//   * @param registry
+//   */
+//  @Override
+//  public void addInterceptors(InterceptorRegistry registry) {
+//    WebContentInterceptor webContentInterceptor = new WebContentInterceptor();
+//    CacheControl nocache = CacheControl.noCache();
+//    webContentInterceptor.addCacheMapping(nocache, "/**");
+//    registry.addInterceptor(webContentInterceptor);
+//    //添加拦截器
+//    registry.addInterceptor(new AppGlobalHandlerInterceptor()).addPathPatterns("/**");
+//    WebMvcConfigurer.super.addInterceptors(registry);
+//
+//  }
+//
+//  @Bean
+//  public HttpMessageConverters fastJsonHttpMessageConverters() {
+//    FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+//    FastJsonConfig fastJsonConfig = new FastJsonConfig();
+//    SerializerFeature[] serializerFeatures = new SerializerFeature[]{
+//        //  输出key是包含双引号
+////        SerializerFeature.QuoteFieldNames,
+//        //  是否输出为null的字段,若为null 则显示该字段
+//     //   SerializerFeature.WriteMapNullValue,
+//        SerializerFeature.WriteNullNumberAsZero,
+//        SerializerFeature.WriteNullListAsEmpty,
+//        SerializerFeature.WriteNullStringAsEmpty,
+//        SerializerFeature.WriteNullBooleanAsFalse,
+//        SerializerFeature.WriteDateUseDateFormat,
+//        SerializerFeature.DisableCircularReferenceDetect,
+//    };
+//
+//    fastJsonConfig.setSerializerFeatures(serializerFeatures);
+//    fastJsonConfig.setCharset(Charset.forName("UTF-8"));
+//    fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+//    fastConverter.setFastJsonConfig(fastJsonConfig);
+//
+//    HttpMessageConverter<?> converter = fastConverter;
+//    List<MediaType> mediaTypes = new ArrayList<>();
+//    mediaTypes.add(MediaType.APPLICATION_JSON_UTF8);//设定json格式且编码为UTF-8
+//    fastConverter.setSupportedMediaTypes(mediaTypes);
+//
+//    return new HttpMessageConverters(converter);
+//  }
 }
