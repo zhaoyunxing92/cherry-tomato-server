6b2afc33cfbbc0dfc642afc5106026b3404f45a2
zhaoyunxing <2385585770@qq.com>
2018-06-09 15:00:24 +0800

    修改：提取appuser公共操作

--- src/main/java/com/sunny/boot/cherrytomato/user/mapper/AppUserMapper.java
+++ src/main/java/com/sunny/boot/cherrytomato/user/mapper/AppUserMapper.java
@@ -3,16 +3,6 @@ package com.sunny.boot.cherrytomato.user.mapper;
 import com.sunny.boot.cherrytomato.common.mapper.BaseMapper;
 import com.sunny.boot.cherrytomato.user.model.AppUser;
 
-public interface AppUserMapper extends BaseMapper {
-    int deleteByPrimaryKey(Long id);
-
-    int insert(AppUser record);
-
-    int insertSelective(AppUser record);
-
-    AppUser selectByPrimaryKey(Long id);
-
-    int updateByPrimaryKeySelective(AppUser record);
-
-    int updateByPrimaryKey(AppUser record);
+public interface AppUserMapper extends BaseMapper<AppUser, Long> {
+  
 }
