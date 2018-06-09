141e3e200ec6ad02d4444c18682a018cfc2ec81b
zhaoyunxing <2385585770@qq.com>
2018-06-09 15:01:38 +0800

    修改：提取根据用户名查询方法

--- src/main/java/com/sunny/boot/cherrytomato/user/mapper/AppUserByUsernameMapper.java
+++ src/main/java/com/sunny/boot/cherrytomato/user/mapper/AppUserByUsernameMapper.java
@@ -3,16 +3,6 @@ package com.sunny.boot.cherrytomato.user.mapper;
 import com.sunny.boot.cherrytomato.common.mapper.BaseMapper;
 import com.sunny.boot.cherrytomato.user.model.AppUserByUsername;
 
-public interface AppUserByUsernameMapper extends BaseMapper {
-    int deleteByPrimaryKey(String username);
-
-    int insert(AppUserByUsername record);
-
-    int insertSelective(AppUserByUsername record);
-
-    AppUserByUsername selectByPrimaryKey(String username);
-
-    int updateByPrimaryKeySelective(AppUserByUsername record);
-
-    int updateByPrimaryKey(AppUserByUsername record);
+public interface AppUserByUsernameMapper extends BaseMapper<AppUserByUsername, String> {
+    
 }
