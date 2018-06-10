0a22f516c832cedbb1d0b91bb80135063970b0f9
zhaoyunxing <2385585770@qq.com>
2018-06-10 23:01:28 +0800

    添加：mapper扫描

--- src/main/java/com/sunny/boot/cherrytomato/CherryTomatoApplication.java
+++ src/main/java/com/sunny/boot/cherrytomato/CherryTomatoApplication.java
@@ -1,12 +1,13 @@
 package com.sunny.boot.cherrytomato;
 
 import com.sunny.boot.cherrytomato.core.config.AppConfig;
-import com.sunny.boot.cherrytomato.core.config.AppWebMvcConfigurerAdapter;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@MapperScan("com.sunny.boot.cherrytomato.**.mapper")
 @Import({AppConfig.class})
 public class CherryTomatoApplication {
 
