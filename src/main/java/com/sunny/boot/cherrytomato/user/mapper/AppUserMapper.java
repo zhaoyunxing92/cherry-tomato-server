0602c4ac79a024e024e683300adf23ab600a58fa
zhaoyunxing <2385585770@qq.com>
2018-06-10 16:58:23 +0800

    修改：mapper添加签名

---
+++ src/main/java/com/sunny/boot/cherrytomato/user/mapper/AppUserMapper.java
@@ -0,0 +1,10 @@
+/**
+ * Copyright(C) 2018 Hangzhou sunny Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.user.mapper;
+
+import com.sunny.boot.cherrytomato.common.mapper.BaseMapper;
+import com.sunny.boot.cherrytomato.user.model.AppUser;
+
+public interface AppUserMapper extends BaseMapper<AppUser, Long> {
+}
