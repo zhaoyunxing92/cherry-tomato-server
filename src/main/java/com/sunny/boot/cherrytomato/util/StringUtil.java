276d9a6722659768c770800ac17598074db18c04
zhaoyunxing <2385585770@qq.com>
2018-06-11 10:52:08 +0800

    修改：邮箱正则变量私有

--- src/main/java/com/sunny/boot/cherrytomato/util/StringUtil.java
+++ src/main/java/com/sunny/boot/cherrytomato/util/StringUtil.java
@@ -16,7 +16,7 @@ public class StringUtil {
   /**
    * 正则表达式：验证邮箱
    */
-  public static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
+  private static final String REGEX_EMAIL = "^([a-z0-9A-Z]+[-|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$";
   /**
    * 正则表达式：验证手机号
    */
