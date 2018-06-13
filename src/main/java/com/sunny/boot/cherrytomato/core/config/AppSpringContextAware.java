64d999b98789a4143e3ba11202a7d87186a1a4b6
zhaoyunxing <2385585770@qq.com>
2018-06-13 13:02:05 +0800

    添加：springContext配置

--- src/main/java/com/sunny/boot/cherrytomato/core/config/AppSpringContextAware.java
+++ src/main/java/com/sunny/boot/cherrytomato/core/config/AppSpringContextAware.java
@@ -3,11 +3,31 @@
  */
 package com.sunny.boot.cherrytomato.core.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 /**
  * @author sunny
  * @class: com.sunny.boot.cherrytomato.core.config.AppSpringContextAware
  * @date: 2018-06-12 14:47
- * @des:
+ * @des: springContext
+ * @see
  */
-public class AppSpringContextAware {
+public class AppSpringContextAware implements ApplicationContextAware {
+  private static ApplicationContext context = null;
+
+  @Override
+  public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+    this.context = applicationContext;
+  }
+
+  /**
+   * 获取当前环境
+   *
+   * @return
+   */
+  public static String[] getActiveProfile() {
+    return context.getEnvironment().getActiveProfiles();
+  }
 }
