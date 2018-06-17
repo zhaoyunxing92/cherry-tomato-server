d11ac0d28607ab13badd42ac95ce042c7fd7767e
zhaoyunxing <2385585770@qq.com>
2018-06-18 02:02:02 +0800

    修改：调整baseModel模块代码

--- src/main/java/com/sunny/boot/cherrytomato/common/model/BaseModel.java
+++ src/main/java/com/sunny/boot/cherrytomato/common/model/BaseModel.java
@@ -11,54 +11,89 @@ import java.util.Date;
  */
 public class BaseModel<ID extends Serializable> implements Serializable {
 
-  private static final long serialVersionUID = 720013901235423189L;
-  private ID id;
+    private static final long serialVersionUID = 720013901235423189L;
+    private ID id;
 
-  private Long creator;
+    /**
+     * 用户状态(1=正常，8=冻结)
+     */
+    private Byte status;
 
-  private Date createDate;
+    /**
+     * 版本
+     */
+    private Integer versions;
 
-  private Long modifier;
+    /**
+     * 创建人
+     */
+    private Long creator;
+    /**
+     * 创建时间
+     */
+    private Date createDate;
+    /**
+     * 修改人
+     */
+    private Long modifier;
+    /**
+     * 修改时间
+     */
+    private Date modifyDate;
 
-  private Date modifyDate;
+    public ID getId() {
+        return id;
+    }
 
-  public ID getId() {
-    return id;
-  }
+    public void setId(ID id) {
+        this.id = id;
+    }
 
-  public void setId(ID id) {
-    this.id = id;
-  }
+    public Byte getStatus() {
+        return status;
+    }
 
-  public Long getCreator() {
-    return creator;
-  }
+    public void setStatus(Byte status) {
+        this.status = status;
+    }
 
-  public void setCreator(Long creator) {
-    this.creator = creator;
-  }
+    public Integer getVersions() {
+        return versions;
+    }
 
-  public Date getCreateDate() {
-    return createDate;
-  }
+    public void setVersions(Integer versions) {
+        this.versions = versions;
+    }
 
-  public void setCreateDate(Date createDate) {
-    this.createDate = createDate;
-  }
+    public Long getCreator() {
+        return creator;
+    }
 
-  public Long getModifier() {
-    return modifier;
-  }
+    public void setCreator(Long creator) {
+        this.creator = creator;
+    }
 
-  public void setModifier(Long modifier) {
-    this.modifier = modifier;
-  }
+    public Date getCreateDate() {
+        return createDate;
+    }
 
-  public Date getModifyDate() {
-    return modifyDate;
-  }
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
 
-  public void setModifyDate(Date modifyDate) {
-    this.modifyDate = modifyDate;
-  }
+    public Long getModifier() {
+        return modifier;
+    }
+
+    public void setModifier(Long modifier) {
+        this.modifier = modifier;
+    }
+
+    public Date getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(Date modifyDate) {
+        this.modifyDate = modifyDate;
+    }
 }
