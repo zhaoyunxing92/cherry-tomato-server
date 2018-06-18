4c843026178bc412b5f5976d2b46b62b419f467a
zhaoyunxing <2385585770@qq.com>
2018-06-18 16:59:42 +0800

    添加：忽略mybatis生成代码配置文件

---
+++ src/main/java/com/sunny/boot/cherrytomato/organization/controller/OrganizationController.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou sunny Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.organization.controller;
+
+/**
+ * @author ${user}
+ * @class: com.sunny.boot.cherrytomato.organization.controller.OrganizationController
+ * @date: 2018-06-18 12:28
+ * @des:
+ */
+public class OrganizationController {
+}
