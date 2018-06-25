b3ebb6107781f4df30421f939c8cd455b7cd6819
zhaoyunxing <2385585770@qq.com>
2018-06-25 17:52:21 +0800

    添加：团队添加成员控制器

--- src/main/java/com/sunny/boot/cherrytomato/organization/controller/OrganizationMemberController.java
+++ src/main/java/com/sunny/boot/cherrytomato/organization/controller/OrganizationMemberController.java
@@ -3,11 +3,39 @@
  */
 package com.sunny.boot.cherrytomato.organization.controller;
 
+import com.sunny.boot.cherrytomato.common.result.Response;
+import com.sunny.boot.cherrytomato.common.valid.InsertGroup;
+import com.sunny.boot.cherrytomato.organization.controller.form.OrgForm;
+import com.sunny.boot.cherrytomato.organization.controller.form.OrgMemberForm;
+import com.sunny.boot.cherrytomato.organization.service.OrganizationMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.organization.controller.OrganizationMemberController
  * @date: 2018-06-25 17:38
- * @des:
+ * @des: 团队成员控制器
  */
+@RestController
+@RequestMapping("/org/member")
 public class OrganizationMemberController {
+    @Autowired
+    private OrganizationMemberService organizationMemberService;
+
+    /**
+     * 团结添加成员
+     *
+     * @param form 成员模块表单
+     * @return 组织id
+     */
+    @PostMapping
+    public Response addOrganizationMember(@RequestBody @Validated({InsertGroup.class}) OrgMemberForm form) {
+        organizationMemberService.addOrganizationMember(form.getOrgId(), form.getUserId());
+        return new Response<Response.Result>(Response.Result.ORG_MEMBER_INSERT_SUCCESS);
+    }
 }
