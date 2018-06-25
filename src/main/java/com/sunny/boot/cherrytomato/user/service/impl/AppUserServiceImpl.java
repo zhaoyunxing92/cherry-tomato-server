2f332cbae565866ce5eff92f74a3794ed26eb643
zhaoyunxing <2385585770@qq.com>
2018-06-25 18:44:00 +0800

    添加：实现根据用户id获取用户接口

--- src/main/java/com/sunny/boot/cherrytomato/user/service/impl/AppUserServiceImpl.java
+++ src/main/java/com/sunny/boot/cherrytomato/user/service/impl/AppUserServiceImpl.java
@@ -3,11 +3,31 @@
  */
 package com.sunny.boot.cherrytomato.user.service.impl;
 
+import com.sunny.boot.cherrytomato.user.mapper.AppUserMapper;
+import com.sunny.boot.cherrytomato.user.model.AppUser;
+import com.sunny.boot.cherrytomato.user.service.AppUserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.user.service.impl.AppUserServiceImpl
  * @date: 2018-06-25 18:25
  * @des:
  */
-public class AppUserServiceImpl {
+@Service
+public class AppUserServiceImpl implements AppUserService {
+    @Autowired
+    private AppUserMapper appUserMapper;
+
+    /**
+     * 根据用户id获取用户信息
+     *
+     * @param userId 用户id
+     * @return 用户信息
+     */
+    @Override
+    public AppUser getAppUser(Long userId) {
+        return appUserMapper.selectAppUserByPrimaryKey(userId);
+    }
 }
