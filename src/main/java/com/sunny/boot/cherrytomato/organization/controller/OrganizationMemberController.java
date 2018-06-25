3a6c3b937a334b58094e8d3eadd40e27b836274b
zhaoyunxing <2385585770@qq.com>
2018-06-25 17:51:27 +0800

    添加：添加团队成员表单

---
+++ src/main/java/com/sunny/boot/cherrytomato/organization/controller/OrganizationMemberController.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.organization.controller;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.organization.controller.OrganizationMemberController
+ * @date: 2018-06-25 17:38
+ * @des:
+ */
+public class OrganizationMemberController {
+}
