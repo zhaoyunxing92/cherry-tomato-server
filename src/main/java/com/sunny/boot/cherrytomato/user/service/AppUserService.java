7fadce238681d9b3954f5f57849607f80a35fc78
zhaoyunxing <2385585770@qq.com>
2018-06-25 18:43:14 +0800

    添加：根据用户id获取用户信息接口

--- src/main/java/com/sunny/boot/cherrytomato/user/service/AppUserService.java
+++ src/main/java/com/sunny/boot/cherrytomato/user/service/AppUserService.java
@@ -3,11 +3,20 @@
  */
 package com.sunny.boot.cherrytomato.user.service;
 
+import com.sunny.boot.cherrytomato.user.model.AppUser;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.user.service.AppUserService
  * @date: 2018-06-25 18:24
- * @des:
+ * @des: 系统用户模块
  */
 public interface AppUserService {
+    /**
+     * 根据用户id获取用户信息
+     *
+     * @param userId
+     * @return
+     */
+    AppUser getAppUser(Long userId);
 }
