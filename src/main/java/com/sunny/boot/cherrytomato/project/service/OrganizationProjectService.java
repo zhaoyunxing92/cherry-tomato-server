e681f0d4b9c5ac4149a1bafc30f3a1705bf8d24b
zhaoyunxing <2385585770@qq.com>
2018-06-26 16:18:21 +0800

    添加：创建项目接口

--- src/main/java/com/sunny/boot/cherrytomato/project/service/OrganizationProjectService.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/service/OrganizationProjectService.java
@@ -3,11 +3,21 @@
  */
 package com.sunny.boot.cherrytomato.project.service;
 
+import com.sunny.boot.cherrytomato.project.controller.from.ProjectForm;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.project.service.OrganizationProjectService
  * @date: 2018-06-26 15:41
- * @des:
+ * @des: 项目服务模块
  */
 public interface OrganizationProjectService {
+    /**
+     * 创建项目
+     *
+     * @param form 项目模块表单
+     * @return 项目id
+     */
+    Long addOrganizationProject(ProjectForm form);
+
 }
