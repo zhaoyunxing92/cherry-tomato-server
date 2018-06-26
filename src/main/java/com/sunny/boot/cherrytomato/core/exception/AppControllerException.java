7896885ca02cafab510f2c92d68f55ddd614c96d
zhaoyunxing <2385585770@qq.com>
2018-06-26 17:06:00 +0800

    调整：项目模块代码

---
+++ src/main/java/com/sunny/boot/cherrytomato/core/exception/AppControllerException.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.core.exception;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.core.exception.AppControllerException
+ * @date: 2018-06-26 16:41
+ * @des:
+ */
+public class AppControllerException {
+}
---
+++ src/main/java/com/sunny/boot/cherrytomato/core/exception/AppServiceException.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.core.exception;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.core.exception.AppServiceException
+ * @date: 2018-06-26 16:33
+ * @des:
+ */
+public class AppServiceException {
+}
