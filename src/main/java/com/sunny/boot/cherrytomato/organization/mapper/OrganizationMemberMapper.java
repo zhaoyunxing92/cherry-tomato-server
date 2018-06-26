74c91994f9458833bc9f928bdf1fbcfa4a77c21b
zhaoyunxing <2385585770@qq.com>
2018-06-26 13:41:29 +0800

    添加：删除成员接口

--- src/main/java/com/sunny/boot/cherrytomato/organization/mapper/OrganizationMemberMapper.java
+++ src/main/java/com/sunny/boot/cherrytomato/organization/mapper/OrganizationMemberMapper.java
@@ -20,4 +20,11 @@ public interface OrganizationMemberMapper extends BaseMapper<OrganizationMember,
      */
     List<Long> selectOrganizationMemberId(@Param("orgId") Long orgId);
 
+    /**
+     * 根据团队id和成员id删除成员
+     *
+     * @param orgId  团队id
+     * @param userId 成员id
+     */
+    void delOrganizationMemberByOrgIdAndUserId(@Param("orgId") Long orgId,@Param("userId") Long userId);
 }
