5ccdc003e770647082a7a06021ff3795611c6e94
zhaoyunxing <2385585770@qq.com>
2018-06-26 11:03:11 +0800

    添加：删除校验组

---
+++ src/main/java/com/sunny/boot/cherrytomato/common/valid/DelGroup.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.common.valid;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.common.valid.DelGroup
+ * @date: 2018-06-26 11:00
+ * @des: 删除校验组
+ */
+public interface DelGroup {
+}
