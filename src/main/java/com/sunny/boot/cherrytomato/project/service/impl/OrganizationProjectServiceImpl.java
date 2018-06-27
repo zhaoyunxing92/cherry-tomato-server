6242c5e99e044d68fd59fd13b9fcf336b9325608
zhaoyunxing <2385585770@qq.com>
2018-06-27 09:07:05 +0800

    添加：实现创建项目接口

--- src/main/java/com/sunny/boot/cherrytomato/project/service/impl/OrganizationProjectServiceImpl.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/service/impl/OrganizationProjectServiceImpl.java
@@ -3,11 +3,55 @@
  */
 package com.sunny.boot.cherrytomato.project.service.impl;
 
+import com.sunny.boot.cherrytomato.common.result.Response;
+import com.sunny.boot.cherrytomato.core.exception.AppServiceException;
+import com.sunny.boot.cherrytomato.organization.model.Organization;
+import com.sunny.boot.cherrytomato.organization.service.OrganizationService;
+import com.sunny.boot.cherrytomato.project.controller.from.ProjectForm;
+import com.sunny.boot.cherrytomato.project.mapper.OrgProjectMapper;
+import com.sunny.boot.cherrytomato.project.model.OrgProject;
+import com.sunny.boot.cherrytomato.project.service.OrganizationProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.project.service.impl.OrganizationProjectServiceImpl
  * @date: 2018-06-26 15:44
- * @des:
+ * @des: 项目服务模块
  */
-public class OrganizationProjectServiceImpl {
+@Service
+public class OrganizationProjectServiceImpl implements OrganizationProjectService {
+    @Autowired
+    private OrganizationService organizationService;
+    @Autowired
+    private OrgProjectMapper orgProjectMapper;
+
+    /**
+     * 创建项目
+     *
+     * @param form
+     * @return 项目id
+     */
+    @Override
+    public Long addOrganizationProject(ProjectForm form) {
+        Long orgId = form.getOrgId();
+        Organization organization = organizationService.getOrganization(orgId);
+        //验证团队是否存在
+        if (Objects.isNull(organization)) {
+            throw new AppServiceException(Response.Result.ORG_IS_NOT_EXIST_ERROR, orgId);
+        }
+        //项目个数是否超限
+
+
+        //创建项目
+        OrgProject project = new OrgProject();
+        project.setOrgId(orgId);
+        orgProjectMapper.insertSelective(project);
+        // 添加成员
+
+        return project.getId();
+    }
 }
