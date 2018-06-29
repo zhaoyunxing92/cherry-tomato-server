945a68827d7880a30bb07c6c9a1be1626efa720a
zhaoyunxing <2385585770@qq.com>
2018-06-29 10:45:31 +0800

    添加：获取团队项目个数方法

--- src/main/java/com/sunny/boot/cherrytomato/project/mapper/OrgProjectMapper.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/mapper/OrgProjectMapper.java
@@ -5,8 +5,16 @@ package com.sunny.boot.cherrytomato.project.mapper;
 
 import com.sunny.boot.cherrytomato.common.mapper.BaseMapper;
 import com.sunny.boot.cherrytomato.project.model.OrgProject;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrgProjectMapper extends BaseMapper<OrgProject, Long> {
+    /**
+     * 根据团队id获取项目个数
+     *
+     * @param orgId
+     * @return
+     */
+    int selectProjectCount(@Param("orgId") Long orgId);
 }
