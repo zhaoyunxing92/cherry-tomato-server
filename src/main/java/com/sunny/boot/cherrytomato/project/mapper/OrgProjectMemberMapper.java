7da8155795836d34bd8ba0b8f657125ea6f912c1
zhaoyunxing <2385585770@qq.com>
2018-06-29 15:15:49 +0800

    添加：获取项目成员接口

--- src/main/java/com/sunny/boot/cherrytomato/project/mapper/OrgProjectMemberMapper.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/mapper/OrgProjectMemberMapper.java
@@ -5,8 +5,18 @@ package com.sunny.boot.cherrytomato.project.mapper;
 
 import com.sunny.boot.cherrytomato.common.mapper.BaseMapper;
 import com.sunny.boot.cherrytomato.project.model.OrgProjectMember;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrgProjectMemberMapper extends BaseMapper<OrgProjectMember, Long> {
+    /**
+     * 获取项目成员id
+     *
+     * @param proId 项目id
+     * @return 项目成员id
+     */
+    List<Long> selectProjectMemberId(@Param("proId") Long proId);
 }
