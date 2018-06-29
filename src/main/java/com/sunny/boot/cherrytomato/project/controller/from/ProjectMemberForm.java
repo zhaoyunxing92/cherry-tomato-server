85986ed3058be27aff30e51eda9c501407930026
zhaoyunxing <2385585770@qq.com>
2018-06-29 14:09:17 +0800

    添加：项目成员表单

--- src/main/java/com/sunny/boot/cherrytomato/project/controller/from/ProjectMemberForm.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/controller/from/ProjectMemberForm.java
@@ -3,6 +3,10 @@
  */
 package com.sunny.boot.cherrytomato.project.controller.from;
 
+import com.sunny.boot.cherrytomato.common.valid.InsertGroup;
+
+import javax.validation.constraints.NotNull;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.project.controller.from.ProjectMemberForm
@@ -10,4 +14,32 @@ package com.sunny.boot.cherrytomato.project.controller.from;
  * @des:
  */
 public class ProjectMemberForm {
+    /**
+     * 项目id
+     */
+    @NotNull(message = "字段【proId】必填", groups = {InsertGroup.class})
+    private Long proId;
+    /**
+     * 成员id
+     */
+    @NotNull(message = "字段【userId】必填", groups = {InsertGroup.class})
+    private Long userId;
+
+    public Long getProId() {
+        return proId;
+    }
+
+    public ProjectMemberForm setProId(Long proId) {
+        this.proId = proId;
+        return this;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public ProjectMemberForm setUserId(Long userId) {
+        this.userId = userId;
+        return this;
+    }
 }
