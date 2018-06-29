9cabef2897f238f6790af541fc11110a96c4ef2e
zhaoyunxing <2385585770@qq.com>
2018-06-29 13:13:56 +0800

    修改：去除org字段

---
+++ src/main/java/com/sunny/boot/cherrytomato/project/service/OrgProjectMemberService.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.project.service;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.project.service.OrgProjectMemberService
+ * @date: 2018-06-29 10:49
+ * @des:
+ */
+public interface OrgProjectMemberService {
+}
