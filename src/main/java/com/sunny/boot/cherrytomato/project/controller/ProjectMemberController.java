c9939e70277366bea1615924612c8a1f5da686f0
zhaoyunxing <2385585770@qq.com>
2018-06-29 14:08:40 +0800

    添加：项目成员控制器

--- src/main/java/com/sunny/boot/cherrytomato/project/controller/ProjectMemberController.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/controller/ProjectMemberController.java
@@ -3,6 +3,15 @@
  */
 package com.sunny.boot.cherrytomato.project.controller;
 
+import com.sunny.boot.cherrytomato.common.result.Response;
+import com.sunny.boot.cherrytomato.common.valid.InsertGroup;
+import com.sunny.boot.cherrytomato.organization.controller.form.OrgMemberForm;
+import com.sunny.boot.cherrytomato.project.controller.from.ProjectMemberForm;
+import com.sunny.boot.cherrytomato.project.service.OrganizationProjectMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/org/project/member")
 public class ProjectMemberController {
+    @Autowired
+    private OrganizationProjectMemberService organizationProjectMemberService;
+    /**
+     * 添加成员
+     *
+     * @param form 成员模块表单
+     * @return 团队信息
+     */
+    @PostMapping
+    public Response addProjectMember(@RequestBody @Validated({InsertGroup.class}) ProjectMemberForm form) {
+        organizationProjectMemberService.addOrgProjectMember(form.getProId(),form.getUserId());
+        return new Response<Response.Result>(Response.Result.ORG_PROJECT_INSERT_MEMBER_SUCCESS);
+    }
 }
