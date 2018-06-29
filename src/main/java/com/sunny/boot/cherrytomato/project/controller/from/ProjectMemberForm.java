a683eecfdde160d99bb0d2180ad44a781247be35
zhaoyunxing <2385585770@qq.com>
2018-06-29 14:03:16 +0800

    添加：项目成员限制

---
+++ src/main/java/com/sunny/boot/cherrytomato/project/controller/from/ProjectMemberForm.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.project.controller.from;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.project.controller.from.ProjectMemberForm
+ * @date: 2018-06-29 13:41
+ * @des:
+ */
+public class ProjectMemberForm {
+}
