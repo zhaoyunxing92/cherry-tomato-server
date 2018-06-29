d506ef973bd0a26d1076b3710a9c1d24c1005b09
zhaoyunxing <2385585770@qq.com>
2018-06-29 13:17:24 +0800

    添加：添加项目成员接口

--- src/main/java/com/sunny/boot/cherrytomato/project/service/OrgProjectMemberService.java
+++ src/main/java/com/sunny/boot/cherrytomato/project/service/OrgProjectMemberService.java
@@ -3,11 +3,30 @@
  */
 package com.sunny.boot.cherrytomato.project.service;
 
+import com.sunny.boot.cherrytomato.common.result.Response;
+
 /**
  * @author zhaoyunxing
  * @class: com.sunny.boot.cherrytomato.project.service.OrgProjectMemberService
  * @date: 2018-06-29 10:49
- * @des:
+ * @des: 项目成员接口
  */
 public interface OrgProjectMemberService {
+    /**
+     * 项目添加成员
+     *
+     * @param proId  项目id
+     * @param userId 用户id
+     * @return
+     */
+    void addOrgProjectMember(Long proId, Long userId);
+
+    /**
+     * 项目添加成员
+     *
+     * @param proId  项目id
+     * @param userId 用户id
+     * @param auto   是否自动添加
+     */
+    void addOrgProjectMember(Long proId, Long userId, Boolean auto);
 }
