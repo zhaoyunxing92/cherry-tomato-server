659062572fa9ad70ffaca5baa8ca4c47f3485821
zhaoyunxing92 <2385585770@qq.com>
2018-07-01 12:43:41 +0800

    添加：团队扩展类

--- src/main/java/com/sunny/boot/cherrytomato/organization/model/vo/OrganizationVo.java
+++ src/main/java/com/sunny/boot/cherrytomato/organization/model/vo/OrganizationVo.java
@@ -3,7 +3,7 @@
  */
 package com.sunny.boot.cherrytomato.organization.model.vo;
 
-import com.sunny.boot.cherrytomato.organization.model.Organization;
+import java.io.Serializable;
 
 /**
  * @author zhaoyunxing
@@ -11,5 +11,55 @@ import com.sunny.boot.cherrytomato.organization.model.Organization;
  * @date: 2018-07-01 11:53
  * @des:
  */
-public class OrganizationVo extends Organization {
+public class OrganizationVo implements Serializable {
+    private static final long serialVersionUID = -7228364830496529889L;
+    /**
+     * 团队id
+     */
+    private Long id;
+    /**
+     * 组织名称
+     */
+    private String name;
+
+    /**
+     * 组织头像
+     */
+    private String logo;
+    /**
+     * 是否超管
+     */
+    private Boolean admin;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
+    }
+
+    public Boolean getAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(Boolean admin) {
+        this.admin = admin;
+    }
 }
