488da6716e58578917ca5f977931a732396b7df9
zhaoyunxing92 <2385585770@qq.com>
2018-07-01 12:30:47 +0800

    添加：获取当前团队

---
+++ src/main/java/com/sunny/boot/cherrytomato/organization/model/vo/OrganizationVo.java
@@ -0,0 +1,15 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.organization.model.vo;
+
+import com.sunny.boot.cherrytomato.organization.model.Organization;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.organization.model.vo.OrganizationVo
+ * @date: 2018-07-01 11:53
+ * @des:
+ */
+public class OrganizationVo extends Organization {
+}
