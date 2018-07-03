1bf437a60b8eec486f3b62902cb3a16f183c073d
zhaoyunxing <2385585770@qq.com>
2018-07-03 09:15:19 +0800

    解决：修复冲突文件

--- src/main/java/com/sunny/boot/cherrytomato/organization/mapper/OrganizationMapper.java
+++ src/main/java/com/sunny/boot/cherrytomato/organization/mapper/OrganizationMapper.java
@@ -6,10 +6,7 @@ package com.sunny.boot.cherrytomato.organization.mapper;
 import com.sunny.boot.cherrytomato.common.mapper.BaseMapper;
 import com.sunny.boot.cherrytomato.organization.model.Organization;
 import com.sunny.boot.cherrytomato.organization.model.vo.OrganizationVo;
-<<<<<<< HEAD
-=======
 import org.apache.ibatis.annotations.Param;
->>>>>>> 911c31910060857dfa9937966c4fc67821350360
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -17,21 +14,12 @@ import java.util.List;
 @Repository
 public interface OrganizationMapper extends BaseMapper<Organization, Long> {
     /**
-<<<<<<< HEAD
      * 根据用户id获取团队列表
      *
      * @param userId
      * @param name
      * @return
      */
-    List<OrganizationVo> selectOrganizationByUserId(Long userId, String name);
-=======
-     * 获取当前登陆人的团队
-     *
-     * @param userId 用户id
-     * @param name   团队名称
-     * @return 团队
-     */
-    List<OrganizationVo> selectCurrentOrganization(@Param("userId") Long userId, @Param("name") String name);
->>>>>>> 911c31910060857dfa9937966c4fc67821350360
+    List<OrganizationVo> selectOrganizationByUserId(@Param("userId") Long userId, @Param("name") String name);
+
 }
