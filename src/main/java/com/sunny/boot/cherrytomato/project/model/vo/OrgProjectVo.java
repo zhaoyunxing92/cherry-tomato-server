4ded312ed464da2f3e95ab0807eb8da07c5ee569
zhaoyunxing92 <2385585770@qq.com>
2018-07-05 23:57:38 +0800

    添加：获取团队项目sql

---
+++ src/main/java/com/sunny/boot/cherrytomato/project/model/vo/OrgProjectVo.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright(C) 2018 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package com.sunny.boot.cherrytomato.project.model.vo;
+
+/**
+ * @author zhaoyunxing
+ * @class: com.sunny.boot.cherrytomato.project.model.vo.OrgProjectVo
+ * @date: 2018-07-05 23:49
+ * @des:
+ */
+public class OrgProjectVo {
+}
