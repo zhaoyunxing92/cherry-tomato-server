ec8ec06574b285b56f5a5fc92109145d7fb74300
zhaoyunxing92 <2385585770@qq.com>
2019-09-20 18:45:44 +0800

    [add]添加配置

---
+++ cherry-tomato-core/src/main/java/io/github/sunny/cherry/tomato/core/common/form/BaseForm.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.sunny.cherry.tomato.core.common.form;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-09-20 14:27
+ * @desc:
+ */
+public class BaseForm {
+}
---
+++ cherry-tomato-core/src/main/java/io/github/sunny/cherry/tomato/core/interceptor/AppGlobalHandlerInterceptor.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.sunny.cherry.tomato.core.interceptor;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-09-20 13:44
+ * @desc:
+ */
+public class AppGlobalHandlerInterceptor {
+}
