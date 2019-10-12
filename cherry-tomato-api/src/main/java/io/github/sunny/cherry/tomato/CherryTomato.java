8851055b82279f75b604cd37242d4abc37b2e8c2
zhaoyunxing92 <2385585770@qq.com>
2019-10-12 12:56:33 +0800

    [up]调整模块依赖关系

--- cherry-tomato-api/src/main/java/io/github/sunny/cherry/tomato/CherryTomato.java
+++ cherry-tomato-api/src/main/java/io/github/sunny/cherry/tomato/CherryTomato.java
@@ -3,26 +3,14 @@
  */
 package io.github.sunny.cherry.tomato;
 
-import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import static org.springframework.core.env.StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME;
-import static org.springframework.core.env.StandardEnvironment.SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME;
 
 /**
  * @author zhaoyunxing
  * @date: 2019-09-20 13:34
  */
 @SpringBootApplication
-@NacosPropertySource(
-        name = "cherry-tomato-api",
-        dataId = "cherry",
-        first = true,
-        autoRefreshed = true,
-        before = SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME,
-        after = SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME
-)
 public class CherryTomato {
 
     public static void main(String[] args) {
