5615a3c90957cc9bd357d3002f89d3164c85b123
zhaoyunxing92 <2385585770@qq.com>
2019-10-16 17:08:57 +0800

    [rm]删除冗余包依赖

--- cherry-tomato-core/src/main/java/io/github/sunny/cherry/tomato/core/utils/ResultUtil.java
+++ cherry-tomato-core/src/main/java/io/github/sunny/cherry/tomato/core/utils/ResultUtil.java
@@ -4,7 +4,6 @@
 package io.github.sunny.cherry.tomato.core.utils;
 
 import io.github.sunny.cherry.tomato.core.result.Response;
-import sun.plugin.navig.motif.OJIPlugin;
 
 /**
  * @author zhaoyunxing
