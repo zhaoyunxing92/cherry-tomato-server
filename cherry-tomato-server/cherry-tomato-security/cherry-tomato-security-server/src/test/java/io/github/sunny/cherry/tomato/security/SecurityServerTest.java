7f7c640ca89c6a3dacf14918af34304f4b14a01d
zhaoyunxing92 <2385585770@qq.com>
2019-10-19 17:09:59 +0800

    [up]密码加密ç±æµ试类

--- cherry-tomato-server/cherry-tomato-security/cherry-tomato-security-server/src/test/java/io/github/sunny/cherry/tomato/security/SecurityServerTest.java
+++ cherry-tomato-server/cherry-tomato-security/cherry-tomato-security-server/src/test/java/io/github/sunny/cherry/tomato/security/SecurityServerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -14,14 +15,15 @@ import static org.junit.Assert.*;
  * @author zhaoyunxing
  * @date: 2019-10-16 12:56
  */
-@RunWith(SpringRunner.class)
-@SpringBootTest
 public class SecurityServerTest {
-    @Autowired
-    private DataSource dataSource;
 
+    /**
+     * 加密码密码
+     */
     @Test
     public void contextLoads() {
-        System.out.println("====>" + dataSource);
+        BCryptPasswordEncoder bcrypt = new BCryptPasswordEncoder();
+        System.out.println(bcrypt.encode("123456"));
+        System.out.println(bcrypt.encode("123456"));
     }
 }
