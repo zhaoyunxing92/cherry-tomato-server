252602510f756a99a56ecdf9c6078c2eca88e5db
zhaoyunxing92 <2385585770@qq.com>
2019-12-21 19:49:06 +0800

    [up]更新配置

---
+++ cherry-tomato-dependencies/cherry-tomato-dubbo/src/main/java/io/github/sunny/dubbo/Information.java
@@ -0,0 +1,11 @@
+/**
+ * Copyright(C) 2019 Hangzhou zhaoyunxing Technology Co., Ltd. All rights reserved.
+ */
+package io.github.sunny.dubbo;
+
+/**
+ * @author zhaoyunxing
+ * @date: 2019-12-21 16:57
+ */
+public interface Information {
+}
